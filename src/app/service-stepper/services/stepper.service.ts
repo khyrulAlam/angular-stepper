bc60b45d09278555c94522855cedc2bbf2c68675
khyrulAlam <khyrulalam69@gmail.com>
2020-09-28 17:28:08 +0600

    data url change

--- src/app/service-stepper/services/stepper.service.ts
+++ src/app/service-stepper/services/stepper.service.ts
@@ -8,10 +8,10 @@ export class StepperService {
   constructor(private _http: HttpClient) { }
 
   getServicesList() {
-    return this._http.get('../assets/data/services-list.json')
+    return this._http.get('https://raw.githubusercontent.com/khyrulAlam/angular-stepper/master/src/assets/data/services-list.json')
   }
 
   getStaffList() {
-    return this._http.get("../assets/data/staff-list.json");
+    return this._http.get("https://raw.githubusercontent.com/khyrulAlam/angular-stepper/master/src/assets/data/staff-list.json");
   }
 }
